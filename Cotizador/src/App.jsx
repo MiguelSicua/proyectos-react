90e1a0ee891027001b4ca8fb00e9faa4f2e0fd17
Miguel Sicua <miguelangelsicua1@gmai.com>
2025-01-29 14:55:43 -0500

    segundo proyecto con react Cotizador

---
+++ Cotizador/src/App.jsx
@@ -0,0 +1,41 @@
+import React, {useState} from "react";
+import Header from "./Components/Header";
+import Formulario from "./Components/Formulario";
+import "./App.css";
+import Resultado from "./Components/resultado";
+import Mensaje from "./Components/Mensaje";
+
+
+function App() {
+  const [cantidad, setCantidad] = useState(0);
+  const [plazo, setPlazo] = useState("");
+  const [total, setTotal] = useState(0);
+ 
+  let componente;
+  if (total === 0) {
+    componente = <Mensaje/>
+    
+  } else {
+    componente = <Resultado total={total} cantidad={cantidad} plazo={plazo} />;
+  }
+
+  return (
+    <>
+      <Header titulo="Cotizador de Prestamos..."/>
+      <div className="container">
+        <Formulario 
+        cantidad={cantidad} 
+        setCantidad={setCantidad}
+        plazo={plazo}
+        setPlazo={setPlazo}
+        setTotal={setTotal}
+        />
+      </div>
+      <div className="mensaje">
+        {componente}
+      </div>
+    </>
+  );
+}
+
+export default App
---
+++ Cotizador/src/Components/Mensaje.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+const Mensaje = () => {
+    return ( 
+        <p>Ingrese una Cantidad y un Plazo</p>
+     );
+}
+ 
+export default Mensaje;
